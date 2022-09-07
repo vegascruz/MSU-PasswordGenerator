e1e6629d7ca04f54e6a50298ea157a1e5e2114a1
Vegascruz <cruzv@students.westerntc.edu>
2022-09-06 22:07:31 -0500

    Added all the prompts, and am able to grab the data. Still working on error handling, which is wonky due to the prompt function... gonna give it a go tomorrow and finish up

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,35 @@
+
 function generatePrompts(){
 
-    let charNum = prompt("Number of Characters between 8-128");
+    //I'm using the prompt function because it wasn't fully clear in the directions.
+    //was gonna use a form and text boxes, but it sounded like "prompts"
+
+    //I tried to do try/catch blocks to handle errors, but got a little messy for
+    //recreating new prompts of the same question 
+    try{
+        var charNum = prompt("Number of Characters between 8-128");
+        if(isNaN(charNum)) {
+            alert("You can only enter in a number");
+            charNum = prompt("Number of Characters between 8-128");
+        } 
+        if((charNum < 8) || (charNum > 128)){
+            alert("Number has to be between 8-128");
+            charNum = prompt("Number of Characters between 8-128");
+        }
+    }catch{
+        console.log(error);
+    }
+    let lowerCaseAnswer = prompt("Include LowerCase? (yes or no)" );
+    let upperCaseAnswer = prompt("Include UpperCase? (yes or no)");
+    let numericAnswer = prompt("Inlude Numeric? (yes or no)");
+    let specialCharAnswer = prompt("Include Special Characters? (yes or no)");
+
+    if( (charNum === "") && (lowerCaseAnswer === "") && (upperCaseAnswer === "") &&
+        (numericAnswer == "") && (specialCharAnswer === ""))
+        {
+            alert("YOU NEED AT LEAST ONE ANSWER");
+            generatePrompts();
+        }
+
 
 }
